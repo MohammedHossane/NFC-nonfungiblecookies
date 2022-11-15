aa42b259fbff5d29a6dbef51845013893d2a7481
Mohammed Hossane <mohammedhossane@Mohammeds-MBP.lan>
2022-11-14 21:47:16 -0800

    fixed one route

--- src/pages/singleCookies/pinkSugar.js
+++ src/pages/singleCookies/pinkSugar.js
@@ -1,10 +1,14 @@
 import Header from "../../components/Header/header";
+import pinksugarcookie from "../../assets/pinksugarcookie.png";
+import "../singleCookies/singleCookies.css"
 
-export default function pinkSugar (){
-    return(
-        <div>
-        <Header></Header>
-        <h1> cookie shi</h1>
-        </div>
-    )
+export default function pinkSugar() {
+  return (
+    <div>
+      <Header></Header>
+      <img className="cookieImg" src={pinksugarcookie} alt="pinksugarcookie" />
+      <h1 id="pinkSugarCookieText">Pink Unicorn Dust</h1>
+      <p className="description">Sugar Cookie with icing made of a pink unicorn!</p>
+    </div>
+  );
 }
